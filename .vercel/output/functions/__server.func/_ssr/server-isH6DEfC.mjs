cacc0f9a3f5782cfa8936fbcf3a050fd3fd208eb
danijimenez <renderdani+github@gmail.com>
2026-06-25 16:42:57 +0200

    removed supabase

--- .vercel/output/functions/__server.func/_ssr/server-isH6DEfC.mjs
+++ .vercel/output/functions/__server.func/_ssr/server-isH6DEfC.mjs
@@ -82,7 +82,7 @@ function getResponse() {
 * the dev styles URL for route-scoped CSS collection.
 */
 async function getStartManifest(matchedRoutes) {
-	const { tsrStartManifest } = await import("../_tanstack-start-manifest_v-yMYG3wmw.mjs");
+	const { tsrStartManifest } = await import("../_tanstack-start-manifest_v-aEStic2i.mjs");
 	const startManifest = tsrStartManifest();
 	let routes = startManifest.routes;
 	routes[rootRouteId];
@@ -1024,7 +1024,7 @@ var createEarlyHintsForRequest = createEarlyHintsCollector;
 async function loadEntries() {
 	const [routerEntry, startEntry, pluginAdapters] = await Promise.all([
 		import("./router-DA3WxaOB.mjs"),
-		import("./start-CAIfq5ov.mjs"),
+		import("./start-Ok9K6Nid.mjs"),
 		import("./empty-plugin-adapters-D9UWiqvJ.mjs")
 	]);
 	return {
--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-aEStic2i.mjs
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-aEStic2i.mjs
@@ -1,24 +1,24 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-yMYG3wmw.js
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-aEStic2i.js
 var tsrStartManifest = () => ({ routes: {
 	__root__: {
 		filePath: "/Users/danijimenez/Documents/projects/powerup-menu/powerup-website-wizard/src/routes/__root.tsx",
 		children: ["/", "/confirmacion"],
-		preloads: ["/assets/index-BzRR2zi8.js"],
+		preloads: ["/assets/index-CarsxOZ2.js"],
 		scripts: [{ attrs: {
 			type: "module",
 			async: !0,
-			src: "/assets/index-BzRR2zi8.js"
+			src: "/assets/index-CarsxOZ2.js"
 		} }]
 	},
 	"/": {
 		filePath: "/Users/danijimenez/Documents/projects/powerup-menu/powerup-website-wizard/src/routes/index.tsx",
 		children: void 0,
-		preloads: ["/assets/routes-lLZFNzmO.js", "/assets/alta.functions-DDpak9Hq.js"]
+		preloads: ["/assets/routes-BzlFqB1Z.js", "/assets/alta.functions-DT7bGP6A.js"]
 	},
 	"/confirmacion": {
 		filePath: "/Users/danijimenez/Documents/projects/powerup-menu/powerup-website-wizard/src/routes/confirmacion.tsx",
 		children: void 0,
-		preloads: ["/assets/confirmacion-wubYdEAW.js", "/assets/alta.functions-DDpak9Hq.js"]
+		preloads: ["/assets/confirmacion-D4t4zoPb.js", "/assets/alta.functions-DT7bGP6A.js"]
 	}
 } });
 //#endregion
